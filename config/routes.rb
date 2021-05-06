8434392ad3754247d12ead452728878a93a315b6
shannamurry <shanna.murry84@gmail.com>
2021-05-05 22:38:31 -0400

    completed assignemnt

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do 
+      # get '/comments', to: 'comments#index', as: 'post_comments'
+      # post '/comments', to: 'comments#create'
+      # get '/comments/new', to: 'comments#new', as: 'new_post_comment'
+      # get '/comments/:id', to: 'comments#show', as: 'post_comment'
+      # get '/comments/:id/edit', to: 'comments#edit', as: 'edit_post_comment'
+      # patch '/comments/:id', to: 'comments#update'
+      # delete '/comments/:id', to: 'comments#destroy'
+      resources :comments
+  end
+
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/pages', to: 'pages#index'
-  # post '/pages', to: 'pages#create'
-  # get '/pages/new', to: 'pages#new', as: 'new_page'
-  # get '/pages/:id', to: 'pages#show', as: 'page'
-  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  # patch '/pages/:id', to: 'pages#update'
-  # delete 'pages/:id', to: 'pages#destroy'
 end
